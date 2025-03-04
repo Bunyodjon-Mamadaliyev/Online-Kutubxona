38ef8b5bc29d39ab7edba19337214faeedb731c9
bunyodjon <bunyodjon.mamadaliyev.2004>
2025-03-04 17:55:38 +0500

    tugallandi

--- Kutubxona/serializers.py
+++ Kutubxona/serializers.py
@@ -15,18 +15,21 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer()
     class Meta:
         model = Book
         fields = ['id', 'title', 'authors', 'genre', 'isbn', 'published_date', 'description', 'page_count', 'language']
 
 
 class BookCopySerializer(serializers.ModelSerializer):
+    book = BookSerializer()
     class Meta:
         model = BookCopy
         fields = ['id', 'book', 'inventory_number', 'condition', 'is_available', 'added_date']
 
 
 class BookLendingSerializer(serializers.ModelSerializer):
+    book_copy = BookCopySerializer()
     class Meta:
         model = BookLending
         fields = ['id', 'book_copy', 'borrower_name', 'borrower_email', 'borrower_date', 'due_date', 'returned_date', 'status']
